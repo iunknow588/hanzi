c3306d9f29c9d10479ff70fa514e344e00f3f505
CDao <iunknow@163.com>
2026-04-02 17:22:25 +0800

    chore: remove build artifacts

--- hanzi-writer-workspace/packages/hanzi-writer-renderers/dist/index.js
+++
@@ -1,5 +0,0 @@
-export * from './HanziWriterRendererBase';
-export * from './RenderTargetBase';
-export * from './StrokeRendererBase';
-export { default as svgRenderer } from './svg';
-export { default as canvasRenderer } from './canvas';
--- hanzi-writer-workspace/packages/hanzi-writer-renderers/dist/svg/index.js
+++
@@ -1,6 +0,0 @@
-import HanziWriterRenderer from './HanziWriterRenderer';
-import RenderTarget from './RenderTarget';
-export default {
-    HanziWriterRenderer: HanziWriterRenderer,
-    createRenderTarget: RenderTarget.init,
-};
